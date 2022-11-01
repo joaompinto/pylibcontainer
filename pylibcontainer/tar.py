3d698653f3d5a0c4eb30b78f08cf42f291606098
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 19:53:06 +0000

    Adding tarfile member sanitization to extractall()

--- pylibcontainer/tar.py
+++ pylibcontainer/tar.py
@@ -23,7 +23,29 @@ def extract_layer(layer_fn):
         )  # Monkey patch mknod because some layers include devices
         with tarfile.open(layer_fn) as image_tar:
             tmp_dir = mkdtemp()
-            image_tar.extractall(tmp_dir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(image_tar, tmp_dir)
         move(tmp_dir, cache_path)
     else:
         print_info("Mounting (cached) read-only rootfs at", cache_path)
